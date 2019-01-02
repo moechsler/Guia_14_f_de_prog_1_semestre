180bd5bf825fdae7cfcec92696f86b2229005309
Marco Oechsler <moechsler@gmail.com>
2019-01-01 21:44:40 -0300

    commit de respaldo guia entregada

---
+++ Guia_14/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Guia_14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.medida1Tb = new System.Windows.Forms.TextBox();
+            this.medida2Tb = new System.Windows.Forms.TextBox();
+            this.medida3Tb = new System.Windows.Forms.TextBox();
+            this.consultaBt = new System.Windows.Forms.Button();
+            this.otraconsultaBt = new System.Windows.Forms.Button();
+            this.salirBt = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tipoTriangulo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(257, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 51);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Triangulo";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // medida1Tb
+            // 
+            this.medida1Tb.Location = new System.Drawing.Point(135, 137);
+            this.medida1Tb.Name = "medida1Tb";
+            this.medida1Tb.Size = new System.Drawing.Size(100, 22);
+            this.medida1Tb.TabIndex = 1;
+            this.medida1Tb.TextChanged += new System.EventHandler(this.medida1Tb_TextChanged);
+            // 
+            // medida2Tb
+            // 
+            this.medida2Tb.Location = new System.Drawing.Point(309, 137);
+            this.medida2Tb.Name = "medida2Tb";
+            this.medida2Tb.Size = new System.Drawing.Size(100, 22);
+            this.medida2Tb.TabIndex = 2;
+            // 
+            // medida3Tb
+            // 
+            this.medida3Tb.Location = new System.Drawing.Point(474, 137);
+            this.medida3Tb.Name = "medida3Tb";
+            this.medida3Tb.Size = new System.Drawing.Size(100, 22);
+            this.medida3Tb.TabIndex = 3;
+            this.medida3Tb.TextChanged += new System.EventHandler(this.medida3Tb_TextChanged);
+            // 
+            // consultaBt
+            // 
+            this.consultaBt.Location = new System.Drawing.Point(621, 136);
+            this.consultaBt.Name = "consultaBt";
+            this.consultaBt.Size = new System.Drawing.Size(75, 23);
+            this.consultaBt.TabIndex = 4;
+            this.consultaBt.Text = "Consultar";
+            this.consultaBt.UseVisualStyleBackColor = true;
+            this.consultaBt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // otraconsultaBt
+            // 
+            this.otraconsultaBt.Location = new System.Drawing.Point(690, 541);
+            this.otraconsultaBt.Name = "otraconsultaBt";
+            this.otraconsultaBt.Size = new System.Drawing.Size(122, 23);
+            this.otraconsultaBt.TabIndex = 5;
+            this.otraconsultaBt.Text = "Otra consulta";
+            this.otraconsultaBt.UseVisualStyleBackColor = true;
+            this.otraconsultaBt.Click += new System.EventHandler(this.otraconsultaBt_Click);
+            // 
+            // salirBt
+            // 
+            this.salirBt.Location = new System.Drawing.Point(830, 541);
+            this.salirBt.Name = "salirBt";
+            this.salirBt.Size = new System.Drawing.Size(75, 23);
+            this.salirBt.TabIndex = 6;
+            this.salirBt.Text = "Salir";
+            this.salirBt.UseVisualStyleBackColor = true;
+            this.salirBt.Click += new System.EventHandler(this.salirBt_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(399, 200);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(280, 262);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(713, 200);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(280, 262);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(1052, 200);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(280, 262);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(77, 200);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(280, 262);
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Lado A";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(306, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Lado B";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(486, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Lado C";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(71, 499);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(424, 32);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Calculo corresponde a triangulo ";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // tipoTriangulo
+            // 
+            this.tipoTriangulo.AutoSize = true;
+            this.tipoTriangulo.BackColor = System.Drawing.Color.Transparent;
+            this.tipoTriangulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipoTriangulo.Location = new System.Drawing.Point(185, 541);
+            this.tipoTriangulo.Name = "tipoTriangulo";
+            this.tipoTriangulo.Size = new System.Drawing.Size(0, 44);
+            this.tipoTriangulo.TabIndex = 15;
+            this.tipoTriangulo.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1369, 648);
+            this.Controls.Add(this.tipoTriangulo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.salirBt);
+            this.Controls.Add(this.otraconsultaBt);
+            this.Controls.Add(this.consultaBt);
+            this.Controls.Add(this.medida3Tb);
+            this.Controls.Add(this.medida2Tb);
+            this.Controls.Add(this.medida1Tb);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Triangulos Marco Oechsler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox medida1Tb;
+        private System.Windows.Forms.TextBox medida2Tb;
+        private System.Windows.Forms.TextBox medida3Tb;
+        private System.Windows.Forms.Button consultaBt;
+        private System.Windows.Forms.Button otraconsultaBt;
+        private System.Windows.Forms.Button salirBt;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label tipoTriangulo;
+    }
+}
+
---
+++ Guia_14/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guia_14
+{
+    public partial class Form1 : Form
+    {
+                                                                                      //variables
+
+        int ladoA, ladoB, ladoC;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Boton consultar
+
+            //valida que no haya textbox vacios
+            if (string.IsNullOrEmpty(medida1Tb.Text))
+            {
+                MessageBox.Show("Debe completar la información");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(medida2Tb.Text))
+            {
+                MessageBox.Show("Debe completar la información");
+                return;
+            }
+            if (string.IsNullOrEmpty(medida3Tb.Text))
+            {
+                MessageBox.Show("Debe completar la información");
+                return;
+            }
+
+                ladoA = int.Parse(medida1Tb.Text);
+                    ladoB = int.Parse(medida2Tb.Text);
+                        ladoC = int.Parse(medida3Tb.Text);
+            
+
+            // validaciones textbox
+
+            if (ladoA == 0)
+            {
+                MessageBox.Show("Lado A no puede ser igual a 0");
+                return;
+            }
+
+            if (ladoB == 0)
+            {
+                MessageBox.Show("Lado B no puede ser igual a 0");
+                return;
+            }
+
+
+
+            if (ladoA + ladoB + ladoC != 180)
+            {
+                MessageBox.Show("Lados deben sumar 180°");
+                return;
+            }
+
+            //Inicio Proceso
+
+
+            //condiciones para evaluar el tipo de triangulo segun los lados ingresados
+
+            if (ladoA == 90 || ladoB == 90 || ladoC == 90)
+            {
+                tipoTriangulo.Text = Convert.ToString("Rectangulo");
+
+                Graphics papel4;//triangulo rectangulo  
+                papel4 = pictureBox4.CreateGraphics();
+                Pen lápiz4 = new Pen(Color.Black);
+
+                papel4.DrawLine(lápiz4, 20, 80, 20, 10);
+                papel4.DrawLine(lápiz4, 20, 10, 120, 80);
+                papel4.DrawLine(lápiz4, 20, 80, 120, 80);
+
+                MessageBox.Show("El triangulo es un Triangulo Rectangulo");
+                return;
+                
+            }
+
+            if (ladoA == ladoB && ladoB != ladoC || ladoB == ladoC && ladoB != ladoA || ladoC == ladoA && ladoC != ladoB)
+            {
+                tipoTriangulo.Text = Convert.ToString("Isosceles");
+
+                Graphics papel2;//triangulo isosceles 
+                papel2 = pictureBox2.CreateGraphics();
+                Pen lápiz2 = new Pen(Color.Black);
+
+                papel2.DrawLine(lápiz2, 50, 80, 70, 10);
+                papel2.DrawLine(lápiz2, 70, 10, 90, 80);
+                papel2.DrawLine(lápiz2, 50, 80, 90, 80);
+
+                MessageBox.Show("El triangulo es un Triangulo Isosceles");
+                return;
+            }
+
+            if (ladoA != ladoB && ladoA != ladoC && ladoB != ladoC)
+            {
+                tipoTriangulo.Text = Convert.ToString("Escaleno");
+
+                Graphics papel3;//triangulo escaleno 
+                papel3 = pictureBox3.CreateGraphics();
+                Pen lápiz3 = new Pen(Color.Black);
+
+                papel3.DrawLine(lápiz3, 20, 80, 40, 40);
+                papel3.DrawLine(lápiz3, 40, 40, 120, 80);
+                papel3.DrawLine(lápiz3, 20, 80, 120, 80);
+
+                MessageBox.Show("El triangulo es un Triangulo Escaleno");
+                return;
+            }
+            if (ladoA == ladoB && ladoA == ladoC)
+            {
+                tipoTriangulo.Text = Convert.ToString("Equilatero");
+
+                Graphics papel;//triangulo equilatero  
+                papel = pictureBox1.CreateGraphics();
+                Pen lápiz = new Pen(Color.Black);
+
+                papel.DrawLine(lápiz, 20, 80, 70, 10);
+                papel.DrawLine(lápiz, 70, 10, 120, 80);
+                papel.DrawLine(lápiz, 20, 80, 120, 80);
+
+                MessageBox.Show("El triangulo es un Triangulo Equilatero");
+                return;
+
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void medida3Tb_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void medida1Tb_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salirBt_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void otraconsultaBt_Click(object sender, EventArgs e)
+        {
+            medida1Tb.Clear();
+            medida2Tb.Clear();
+            medida3Tb.Clear();
+            tipoTriangulo.Text = String.Empty;
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            pictureBox3.Image = null;
+            pictureBox4.Image = null;
+            medida1Tb.Focus();
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
